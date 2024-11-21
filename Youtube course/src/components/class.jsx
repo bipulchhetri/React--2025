4fabd1bc2f81afa0f0740361e2d8ee4d22afeb94
bipulchhetri <bipulchhetri80@gmail.com>
2024-11-21 19:30:57 +0530

    ALL OK

---
+++ Youtube course/src/components/class.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+class ClassBasedComponent extends Component {
+    render() {
+        return
+    (
+        <div>
+            <h3>Hello Class based Component</h3>
+        </div>
+    )
+}
+}
+export default ClassBasedComponent;
---
+++ Youtube course/src/components/fun-com.jsx
@@ -0,0 +1,8 @@
+import { Component } from "react";
+function fun(){
+    return(
+        <div> 
+            <h4>Hello hgow are yourß</h4>
+        </div>
+    )
+}export default fun-com;
