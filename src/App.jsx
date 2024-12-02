6bf46eeaf3346316bc2a2b189eb5642fd1108e98
bipulchhetri <bipulchhetri80@gmail.com>
2024-12-02 08:28:09 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Course from "./component/Course";
 import { EventHandler } from "./component/EventHandler";
 import { Eventprop } from "./component/Eventprop";
+import StateL from "./component/StateL";
+import { Toggle } from "./Projects/Toggle";
 // import { Eventchild, Eventprop } from "./component/Eventprop";
 
 export const App = () => {
@@ -10,10 +12,12 @@ export const App = () => {
   {/* <Course name={'Eye of Tears'} rating={`Rating 5.2`} />
   <Course name={'Eye of Man'} rating={`Rating 5.2`} /> */}
 
-<Course/>
-<EventHandler/>
+{/* <Course/> */}
+{/* <EventHandler/> */}
 {/* <Eventchild/> */}
-<Eventprop/>
+{/* <Eventprop/> */}
+<StateL/>
+<Toggle/>
     </>
   );
 };
--- src/component/StateL.jsx
+++ src/component/StateL.jsx
@@ -1,14 +1,30 @@
 import React, { useState } from 'react'
 import './State.css'
 const StateL = () => {
-    const [text,setText]=useState('');
-   const handleclick=(event)=>{
-    setText(event.target.value)
-   }
+  //   const [text,setText]=useState('');
+  //  const handleclick=(event)=>{
+  //   setText(event.target.value)
+  //  }
+  const [count,setCount]=useState(0);
+  const handleClick =()=>{
+setCount (count+1);
+  }
+  const handleClickDe=()=>{
+  setCount(count > 0 ? count - 1 : 0)
+
+  }
+  const Reset=()=>{
+    setCount(0)
+  }
   return (
     <>
-    <input type="text" value={text} onChange={handleclick} placeholder='Type something' />
-    <p>Your Text- {text}</p>
+    {/* <input type="text" value={text} onChange={handleclick} placeholder='Type something' />
+    <p>Your Text- {text}</p> */}
+    <p>{count}</p>
+    <button onClick={handleClick}> Increment</button>
+    <button onClick={handleClickDe}> Decrement</button>
+    <button onClick={Reset}>Reset</button>
+
     </>
   )
 }
