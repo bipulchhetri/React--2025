04334c93e992a071a8102e627330669ef081456b
bipulchhetri <bipulchhetri80@gmail.com>
2024-12-14 22:39:56 +0530

    UseState revised

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,60 @@
-import React from "react";
-import Course from "./component/Course";
-import { EventHandler } from "./component/EventHandler";
-import { Eventprop } from "./component/Eventprop";
-import StateL from "./component/StateL";
-import { Toggle } from "./Projects/Toggle";
-import HookCh from "./Projects/HookCh";
-import Registration from "./Projects/Registration";
-import { Login } from "./Projects/Login";
-import Propdriling from "./component/Propdriling";
-import { Bio, Bioprovider } from "./Hooks/ContextApi";
-import Home from "./Hooks/ContextApi/Home";
-import CrudComponent from "./Projects/CrudComponent";
-// import { Eventchild, Eventprop } from "./component/Eventprop";
+// import React from "react";
+// import Course from "./component/Course";
+// import { EventHandler } from "./component/EventHandler";
+// import { Eventprop } from "./component/Eventprop";
+// import StateL from "./component/StateL";
+// import { Toggle } from "./Projects/Toggle";
+// import HookCh from "./Projects/HookCh";
+// import Registration from "./Projects/Registration";
+// import { Login } from "./Projects/Login";
+// import Propdriling from "./component/Propdriling";
+// import { Bio, Bioprovider } from "./Hooks/ContextApi";
+// import Home from "./Hooks/ContextApi/Home";
+// import CrudComponent from "./Projects/CrudComponent";
+// // import { Eventchild, Eventprop } from "./component/Eventprop";
 
-export const App = () => {
-  return (
-    <>
-  {/* <Course name={'Eye of Tears'} rating={`Rating 5.2`} />
-  <Course name={'Eye of Man'} rating={`Rating 5.2`} /> */}
+// export const App = () => {
+//   return (
+//     <>
+//   {/* <Course name={'Eye of Tears'} rating={`Rating 5.2`} />
+//   <Course name={'Eye of Man'} rating={`Rating 5.2`} /> */}
 
-{/* <Course/> */}
-{/* <EventHandler/> */}
-{/* <Eventchild/> */}
-{/* <Eventprop/> */}
-{/* <StateL/>
-<Toggle/> */}
-{/* <HookCh/> */}
-{/* <Registration/> */}
-{/* <Login/> */}
-{/* <Propdriling/> */}
-{/* <Bioprovider>
-  <Home/>
+// {/* <Course/> */}
+// {/* <EventHandler/> */}
+// {/* <Eventchild/> */}
+// {/* <Eventprop/> */}
+// {/* <StateL/>
+// <Toggle/> */}
+// {/* <HookCh/> */}
+// {/* <Registration/> */}
+// {/* <Login/> */}
+// {/* <Propdriling/> */}
+// {/* <Bioprovider>
+//   <Home/>
   
-</Bioprovider> */}
-<CrudComponent/>
-    </>
-  );
-};
+// </Bioprovider> */}
+// <CrudComponent/>
+//     </>
+//   );
+// };
 
 
-export default App;
+// export default App;
+
+
+// Revsion App started
+
+import React from 'react'
+import UseState from './Hookes-All/UseState'
+
+const App = () => {
+  return (
+  
+<>
+<UseState/>
+</>
+
+  )
+}
+
+export default App
---
+++ src/Hookes-All/UseState.jsx
@@ -0,0 +1,20 @@
+// import React, { useState } from 'react'
+
+// const UseState=()=>{
+//     // ******* Counter app example •••••
+//     const [Count,setCount]=useState(0)
+//     const increment=()=> setCount(Count<10 ? Count+1: Count);
+//     const Decrement=()=> setCount(Count>0 ? Count-1 :0);
+//     return(
+       
+//         <>
+//          <div>{Count}</div>
+//          <button onClick={increment} >Increment</button>
+//          <button onClick={Decrement} >Decrement</button>
+//         </>
+//     )
+// }
+// export default UseState;
+
+// ••••• form using •••USESTATE•••
+
