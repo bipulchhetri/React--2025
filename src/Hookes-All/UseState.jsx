faa0c05ea43c11cff07adcbca68e1bdbad97ce1e
bipulchhetri <bipulchhetri80@gmail.com>
2024-12-21 22:46:54 +0530

    updated usestate

--- src/Hookes-All/UseState.jsx
+++ src/Hookes-All/UseState.jsx
@@ -6,7 +6,7 @@
 //     const increment=()=> setCount(Count<10 ? Count+1: Count);
 //     const Decrement=()=> setCount(Count>0 ? Count-1 :0);
 //     return(
-       
+
 //         <>
 //          <div>{Count}</div>
 //          <button onClick={increment} >Increment</button>
@@ -17,4 +17,30 @@
 // export default UseState;
 
 // ••••• form using •••USESTATE•••
+import { useState } from "react";
+const UseState = () => {      
+    const [email,setEmail]=useState('')
+    const [name,setName]=useState('')
+    const handlesubmit=(e)=>{
+        e.preventDefault();
 
+        alert(`submitted ${name }  and ${email}`)
+    }
+  return (
+    <>
+      <form onSubmit={handlesubmit}>
+
+      <input type="text" id="name" value={name} onChange={(e)=>setName(e.target.value)} />
+      <input type="email" id="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+ <button type="submit" >Submit</button>
+      </form>
+
+      <div>
+        <p>{name}</p>
+        <p>{email}</p>
+      </div>
+    </>
+
+  );
+};
+export default UseState;
