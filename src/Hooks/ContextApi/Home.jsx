7f82d160a00926583fb8ec58a39670d0cf20e912
bipulchhetri <bipulchhetri80@gmail.com>
2025-01-03 21:47:20 +0530

    context api

--- src/Hooks/ContextApi/Home.jsx
+++ src/Hooks/ContextApi/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useContext } from 'react'
-import { BioContext } from '.'
+import { Bio } from '.'
 
 export const Home = () => {
 
-    const {MyName,age}=useContext(BioContext);
-    return <h1>Hello My Name is {MyName} and age {age}</h1>
+    const {name,age}=useContext({Bio});
+    return <h1>Hello My Name is {name} and age {age}</h1>
 
 }
 
--- src/Hooks/ContextApi/index.jsx
+++ src/Hooks/ContextApi/index.jsx
@@ -1,16 +1,30 @@
+// import { createContext } from "react";
+
 import { createContext } from "react";
 
-// 1st step
-export const BioContext=createContext();
+// // 1st step
+// export const BioContext=createContext();
 
-// 2nd step
+// // 2nd step
 
-export const BioProvider=({children})=>{
-    const MyName="Bipul"
-    const age="32"
+// export const BioProvider=({children})=>{
+//     const MyName="Bipul"
+//     const age="32"
+//     return(
+//         <BioContext.Provider value={{MyName,age}}>
+// {children}
+//         </BioContext.Provider>
+//     )
+// }
+
+export const Bio=createContext()
+
+export const BioProvider=({Children})=>{
+    const name="Bipul"
+   const age="32"
     return(
-        <BioContext.Provider value={{MyName,age}}>
-{children}
-        </BioContext.Provider>
+        <Bio.Provider value={{name,age}}>
+{Children}
+        </Bio.Provider>
     )
 }
